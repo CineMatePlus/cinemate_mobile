023957a6804658b8d322df681ac386ddf8396a64
npcBerk <muhammetberk561@gmail.com>
2025-04-15 11:10:10 +0300

    Gereksiz sınıflar temizlendi

--- lib/core/providers/api_service_provider.dart
+++ lib/core/providers/api_service_provider.dart
@@ -1,15 +1,9 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import '../services/api_service.dart';
-import '../services/auth_service.dart';
 
 part 'api_service_provider.g.dart';
 
 @riverpod
 ApiService apiService(ApiServiceRef ref) {
   return ApiService();
-}
-
-@riverpod
-AuthService authService(AuthServiceRef ref) {
-  return AuthService(ref);
 }
